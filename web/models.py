76519a558abe323dc35f7a7acec077d8b7b93fe8
penysho <penysho@gmail.com>
2024-04-13 20:04:25 +0900

    feat: siteモデル初期定義

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Site(models.Model):
+
+    site_id = models.BigAutoField(primary_key=True)
+    site_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = "site"
+
+    def __str__(self):
+        return self.site_id
