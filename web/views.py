69f6a783985599cf203c90211922d3910ea03211
penysho <penysho@gmail.com>
2024-04-13 19:57:36 +0900

    feat: IndexView作成

--- web/views.py
+++ web/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "web/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
