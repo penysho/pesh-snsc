75d6824dc24b22f9e6fe1b00bc8e4f312059824f
penysho <penysho@gmail.com>
2024-04-14 15:38:35 +0900

    feat: サイト所有権紐付けテーブル初期定義

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from web.models import Site
+
 # Register your models here.
 
 SnscUser = get_user_model()
 
 admin.site.register(SnscUser)
+admin.site.register(Site)
--- web/models.py
+++ web/models.py
@@ -127,11 +127,23 @@ class Site(models.Model):
     is_active = models.BooleanField(
         default=False, verbose_name="アクティブなサイトか否か"
     )
+    snsc_users = models.ManyToManyField(SnscUser, through="SiteOwnership")
     created_at = models.DateTimeField(default=timezone.now, verbose_name="登録日")
     updated_at = models.DateTimeField(default=timezone.now, verbose_name="更新日")
 
     class Meta:
         db_table = "site"
+        verbose_name = "サイトマスタ"
+        verbose_name_plural = "サイトマスタ"
 
-    def __str__(self):
-        return self.site_id
+
+class SiteOwnership(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    snsc_user = models.ForeignKey(SnscUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="登録日")
+    updated_at = models.DateTimeField(default=timezone.now, verbose_name="更新日")
+
+    class Meta:
+        db_table = "site_ownership"
+        verbose_name = "サイト所有権紐付けテーブル"
+        verbose_name_plural = "サイト所有権紐付けテーブル"
