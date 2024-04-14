3292254d62676ad5e50eebd46cd963cd041527f8
penysho <penysho@gmail.com>
2024-04-14 11:15:02 +0900

    feat: postgre初期設定

--- snsc/settings.py
+++ snsc/settings.py
@@ -13,20 +13,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o_wxnx&kb2#61u+vfo(h_!w=mdr$_^*r*zzibem+a$^269%q4j"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+env.read_env(os.path.join(BASE_DIR, ".env"))
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
@@ -75,10 +75,7 @@ WSGI_APPLICATION = "snsc.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db(),
 }
 
 
--- web/models.py
+++ web/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 
 class Site(models.Model):
-
     site_id = models.BigAutoField(primary_key=True)
     site_name = models.CharField(max_length=50)
     created_at = models.DateTimeField(default=timezone.now)
