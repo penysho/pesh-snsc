424de8abff18c649917515f9ce64650989155c3e
penysho <penysho@gmail.com>
2024-04-30 15:43:03 +0900

    fix: baseモデル自動日時更新定義変更

--- web/models/snsc_base_model.py
+++ web/models/snsc_base_model.py
@@ -4,16 +4,8 @@ from django.utils import timezone
 
 class SnscBaseModel(models.Model):
     is_active = models.BooleanField(default=False, verbose_name="使用フラグ")
-    created_at = models.DateTimeField(default=timezone.now, verbose_name="登録日")
-    updated_at = models.DateTimeField(default=timezone.now, verbose_name="更新日")
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.created_at = timezone.now()
-            self.updated_at = timezone.now()
-        else:
-            self.updated_at = timezone.now()
-        super().save(*args, **kwargs)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日")
 
     class Meta:
         abstract = True
