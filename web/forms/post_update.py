4c88ad68972eb3b32bd31cd0df87a3ee9eeeeeae
penysho <penysho@gmail.com>
2024-06-02 17:33:48 +0900

    feat: 投稿詳細ページのform作成

--- web/forms/post_update.py
+++ web/forms/post_update.py
@@ -4,7 +4,20 @@ from web.models import Post
 
 
 class PostUpdateForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "投稿タイトルを入力してください。",
+                "class": "w-full text-lg form__item",
+            }
+        )
+    )
+
+    status = forms.ChoiceField(
+        choices=Post.Status,
+        widget=forms.Select(attrs={"class": "form__item"}),
+    )
 
     class Meta:
         model = Post
-        fields = ("title",)
+        fields = ("title", "status")
--- web/views/post_update.py
+++ web/views/post_update.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views import generic
 
 from core.session.implements.session import SnscSessionImpl
@@ -13,6 +14,7 @@ class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = get_template_name("post_update.html")
     model = Post
     form_class = PostUpdateForm
+    success_url = reverse_lazy("web:post_list")
 
     def get_queryset(self):
         return PostUpdateServiceImpl(post_repository=PostRepositoryImpl()).get_queryset(
